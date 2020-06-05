d8289641db60b434bb5b21b389eaa1f3439f0117
lickthecheese <lickthecheese@tilde.team>
2020-06-05 12:06:51 -0400

    module manager

--- modules/modulemanager.py
+++ modules/modulemanager.py
@@ -1,7 +1,7 @@
 
 
-async def listMods(s,c,n,m):
-  pass
+async def listMods(self,c,n,m):
+  await self.message(c,'[\x036modulemanager\x0f] currently loaded mods: {}'.format(list(self.modules.keys())))
 
 async def init(self):
   self.help['modules'] = ['modules - list the modules']
