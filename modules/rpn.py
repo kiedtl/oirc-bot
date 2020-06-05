30e0cfeb81922e568974abdacdd96eefdae5e025
lickthecheese <lickthecheese@tilde.team>
2020-06-05 16:40:07 -0400

    print

--- modules/rpn.py
+++ modules/rpn.py
@@ -25,13 +25,15 @@ async def rpninp(self, chan, nick, msg):
     self.rpnhist[chan][0] =  self.rpnhist[chan].pop(1)*self.rpnhist[chan][0]
   elif msg == '/' or msg == 'd':
     self.rpnhist[chan][0] =  self.rpnhist[chan].pop(1)/self.rpnhist[chan][0]
+  elif msg == 'p':
+    pass # just dont do anything lol
   else:
     self.rpnhist[chan].insert(0, self.rpnhist[chan][0])
   await self.message(chan, '[\x036rpn\x0f] '+str(self.rpnhist[chan][0]))
     
 
 async def init(self):
-  self.help['rpn'] = ['rpn <inp> - simple reverse polish notation calculator (more)', 'it has an alias of . so you can just do {}. <inp> and also there are 4 f8jct90js (+|a) (-|s) (*|x|m) (/|d)']
+  self.help['rpn'] = ['rpn <inp> - simple reverse polish notation calculator (more)', 'it has an alias of . so you can just do {}. <inp> and also there are 4 f8jct90js (+|a) (-|s) (*|x|m) (/|d) and p to print register 0']
   self.cmd['rpn'] = rpninp
   self.cmd['.'] = rpninp
   
