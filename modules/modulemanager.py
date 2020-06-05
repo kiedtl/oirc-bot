98d8324e29399403f2da56fc8547edbe5efef0ff
lickthecheese <lickthecheese@tilde.team>
2020-06-05 16:37:30 -0400

    simple rpn calculator

--- modules/modulemanager.py
+++ modules/modulemanager.py
@@ -4,6 +4,6 @@ async def listMods(self,c,n,m):
   await self.message(c,'[\x036modulemanager\x0f] currently loaded mods: {}'.format(list(self.modules.keys())))
 
 async def init(self):
-  self.help['modules'] = ['modules - list the modules']
+  self.help['modules'] = ['modules - list the modules',':o']
   self.cmd['modules'] = listMods
 
--- modules/rpn.py
+++ modules/rpn.py
@@ -2,7 +2,38 @@
 import subprocess
 
 
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+
+async def rpninp(self, chan, nick, msg):
+  if chan not in self.rpnhist:
+    self.rpnhist[chan] = [0]
+  self.rpnhist[chan].append(0)
+  del self.rpnhist[chan][100:]
+  if isfloat(msg):
+    self.rpnhist[chan].insert(0, float(msg))
+  elif msg == '+' or msg == 'a':
+    self.rpnhist[chan][0] = self.rpnhist[chan][0]+self.rpnhist[chan].pop(1)
+  elif msg == '-' or msg == 's':
+    self.rpnhist[chan][0] =  self.rpnhist[chan].pop(1)-self.rpnhist[chan][0]
+  elif msg == '*' or msg == 'x' or msg == 'm':
+    self.rpnhist[chan][0] =  self.rpnhist[chan].pop(1)*self.rpnhist[chan][0]
+  elif msg == '/' or msg == 'd':
+    self.rpnhist[chan][0] =  self.rpnhist[chan].pop(1)/self.rpnhist[chan][0]
+  else:
+    self.rpnhist[chan].insert(0, self.rpnhist[chan][0])
+  await self.message(chan, '[\x036rpn\x0f] '+str(self.rpnhist[chan][0]))
+    
 
 async def init(self):
-  pass
-
+  self.help['rpn'] = ['rpn <inp> - simple reverse polish notation calculator (more)', 'it has an alias of . so you can just do {}. <inp> and also there are 4 f8jct90js (+|a) (-|s) (*|x|m) (/|d)']
+  self.cmd['rpn'] = rpninp
+  self.cmd['.'] = rpninp
+  
+  self.rpnhist = {}
+  
